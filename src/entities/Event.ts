ba7cf04dcafd8d6080d8737374fcfe498dbcf5eb
Solomon <63205747+solomonNSI@users.noreply.github.com>
2024-07-05 10:12:08 +0300

    Revert "added sender/receiver/amount columns"
    This reverts commit e30a382e17a8b905b014318289eb5415ba64c836.

--- src/entities/Event.ts
+++ src/entities/Event.ts
@@ -1,10 +1,4 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  Index,
-  PrimaryGeneratedColumn,
-} from "typeorm";
+import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 @Index(["transactionHash", "logIndex"], { unique: true })
@@ -37,20 +31,11 @@ export class Event {
   parsedData: string;
 
   @Column()
-  sender: string;
-
-  @Column()
-  receiver: string;
-
-  @Column()
-  amountTransferred: string;
-
-  @Column()
   contractAddress: string;
 
   @Column()
   appName: string;
 
-  @CreateDateColumn({ type: "timestamp" })
+  @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
 }
